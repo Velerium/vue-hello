30e2a955a02ea4c490f920f6efbbe09ce70737ed
Velerium <bondaniandrea@libero.it>
2021-06-24 16:40:25 +0200

    Good luck to us...

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            text: 'Hello World!!',
+            linkImg1: 'https://img.freepik.com/free-vector/colorful-palm-silhouettes-background_23-2148541792.jpg?size=626&ext=jpg'
+        }
+    }
+)
